99f787a0e24ed96950e4dc6f26050b95640ae6a2
FellowTraveler <F3llowTraveler@gmail.com>
2023-07-30 23:01:22 -0700

    rpc/Rpc and rpc/RpcAsync migrated

---
+++ tests/ottest/fixtures/rpc/RpcAsync.cpp
@@ -0,0 +1,259 @@
+// Copyright (c) 2010-2023 The Open-Transactions developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ottest/fixtures/rpc/RpcAsync.hpp"  // IWYU pragma: associated
+
+#include <APIArgument.pb.h>
+#include <AcceptPendingPayment.pb.h>
+#include <AccountEvent.pb.h>
+#include <PaymentWorkflowEnums.pb.h>
+#include <RPCCommand.pb.h>
+#include <RPCEnums.pb.h>
+#include <RPCPush.pb.h>
+#include <RPCResponse.pb.h>
+#include <RPCStatus.pb.h>
+#include <RPCTask.pb.h>
+#include <SendPayment.pb.h>
+#include <TaskComplete.pb.h>
+#include <gtest/gtest.h>
+#include <opentxs/opentxs.hpp>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#include <ctime>
+#include <functional>
+#include <future>
+#include <iosfwd>
+#include <memory>
+#include <utility>
+
+#include "internal/serialization/protobuf/Check.hpp"
+#include "internal/serialization/protobuf/verify/RPCPush.hpp"
+#include "internal/serialization/protobuf/verify/RPCResponse.hpp"
+#include "ottest/Basic.hpp"
+#include "ottest/fixtures/common/Base.hpp"
+
+namespace ottest
+{
+namespace ot = opentxs;
+
+int RpcAsync::sender_session_{0};
+int RpcAsync::receiver_session_{0};
+identifier::Generic RpcAsync::destination_account_id_{
+    ot::identifier::Generic::Factory()};
+int RpcAsync::intro_server_{0};
+std::unique_ptr<OTZMQListenCallback> RpcAsync::notification_callback_{nullptr};
+std::unique_ptr<OTZMQSubscribeSocket> RpcAsync::notification_socket_{nullptr};
+identifier::Nym RpcAsync::receiver_nym_id_{ot::identifier::Nym::Factory()};
+identifier::Nym RpcAsync::sender_nym_id_{ot::identifier::Nym::Factory()};
+int RpcAsync::server_{0};
+identifier::UnitDefinition RpcAsync::unit_definition_id_{
+    ot::identifier::UnitDefinition::Factory()};
+identifier::Generic RpcAsync::workflow_id_{ot::identifier::Generic::Factory()};
+identifier::Notary RpcAsync::intro_server_id_{
+    ot::identifier::Notary::Factory()};
+identifier::Notary RpcAsync::server_id_{ot::identifier::Notary::Factory()};
+RpcAsync::PushChecker RpcAsync::push_checker_{};
+std::promise<ot::UnallocatedVector<bool>> RpcAsync::push_received_{};
+UnallocatedVector<bool> RpcAsync::push_results_{};
+std::size_t RpcAsync::push_results_count_{0};
+
+RpcAsync::RpcAsync()
+{
+    if (false == bool(notification_callback_)) {
+        notification_callback_.reset(new OTZMQListenCallback(
+            ot::network::zeromq::ListenCallback::Factory(
+                [](const ot::network::zeromq::Message&& incoming) -> void {
+                    process_notification(incoming);
+                })));
+    }
+    if (false == bool(notification_socket_)) {
+        notification_socket_.reset(new OTZMQSubscribeSocket(
+            ot_.ZMQ().SubscribeSocket(*notification_callback_)));
+    }
+}
+
+bool RpcAsync::check_push_results(const ot::UnallocatedVector<bool>& results)
+{
+    return std::all_of(
+        results.cbegin(), results.cend(), [](bool result) { return result; });
+}
+
+proto::RPCCommand RpcAsync::init(proto::RPCCommandType commandtype)
+{
+    auto cookie = ot::identifier::Generic::Random()->str();
+
+    proto::RPCCommand command;
+    command.set_version(COMMAND_VERSION);
+    command.set_cookie(cookie);
+    command.set_type(commandtype);
+
+    return command;
+}
+
+std::future<ot::UnallocatedVector<bool>> RpcAsync::set_push_checker(
+    PushChecker func,
+    std::size_t count = 1)
+{
+    push_checker_ = func;
+    push_results_count_ = count;
+    push_received_ = {};
+
+    return push_received_.get_future();
+}
+
+void RpcAsync::cleanup()
+{
+    notification_socket_->get().Close();
+    notification_socket_.reset();
+    notification_callback_.reset();
+}
+
+std::size_t RpcAsync::get_index(const std::int32_t instance)
+{
+    return (instance - (instance % 2)) / 2;
+}
+
+const api::Session& RpcAsync::get_session(const std::int32_t instance)
+{
+    auto is_server = instance % 2;
+
+    if (is_server) {
+        return OTTestEnvironment::GetOT().Server(
+            static_cast<int>(get_index(instance)));
+    } else {
+        return OTTestEnvironment::GetOT().ClientSession(
+            static_cast<int>(get_index(instance)));
+    }
+}
+
+void RpcAsync::process_notification(
+    const ot::network::zeromq::Message&& incoming)
+{
+    if (1 < incoming.Body().size()) { return; }
+
+    const auto& frame = incoming.Body().at(0);
+    const auto rpcpush = proto::Factory<proto::RPCPush>(frame);
+
+    if (push_checker_) {
+        push_results_.emplace_back(push_checker_(rpcpush));
+        if (push_results_.size() == push_results_count_) {
+            push_received_.set_value(push_results_);
+            push_checker_ = {};
+            push_received_ = {};
+            push_results_ = {};
+        }
+    } else {
+        try {
+            push_received_.set_value(ot::UnallocatedVector<bool>{false});
+        } catch (...) {
+        }
+
+        push_checker_ = {};
+        push_received_ = {};
+        push_results_ = {};
+    }
+}
+
+bool RpcAsync::default_push_callback(const ot::proto::RPCPush& push)
+{
+    if (false == proto::Validate(push, VERBOSE)) { return false; }
+
+    if (proto::RPCPUSH_TASK != push.type()) { return false; }
+
+    auto& task = push.taskcomplete();
+
+    if (false == task.result()) { return false; }
+
+    if (proto::RPCRESPONSE_SUCCESS != task.code()) { return false; }
+
+    return true;
+}
+
+void RpcAsync::setup()
+{
+    const api::Context& ot = OTTestEnvironment::GetOT();
+
+    auto& intro_server = ot.StartNotarySession(
+        ArgList(), static_cast<int>(ot.NotarySessionCount()), true);
+    auto& server = ot.StartNotarySession(
+        ArgList(), static_cast<int>(ot.NotarySessionCount()), true);
+    auto reasonServer = server.Factory().PasswordPrompt(__func__);
+    intro_server.SetMintKeySize(OT_MINT_KEY_SIZE_TEST);
+    server.SetMintKeySize(OT_MINT_KEY_SIZE_TEST);
+    auto server_contract = server.Wallet().Server(server.ID());
+    intro_server.Wallet().Server(server_contract->PublicContract());
+    server_id_ = ot::identifier::Notary::Factory(server_contract->ID()->str());
+    auto intro_server_contract =
+        intro_server.Wallet().Server(intro_server.ID());
+    intro_server_id_ =
+        ot::identifier::Notary::Factory(intro_server_contract->ID()->str());
+    auto cookie = ot::identifier::Generic::Random()->str();
+    proto::RPCCommand command;
+    command.set_version(COMMAND_VERSION);
+    command.set_cookie(cookie);
+    command.set_type(proto::RPCCOMMAND_ADDCLIENTSESSION);
+    command.set_session(-1);
+    auto response = ot.RPC(command);
+
+    ASSERT_TRUE(proto::Validate(response, VERBOSE));
+    ASSERT_EQ(1, response.status_size());
+    ASSERT_EQ(proto::RPCRESPONSE_SUCCESS, response.status(0).code());
+
+    auto& senderClient =
+        ot.Client(static_cast<int>(get_index(response.session())));
+    auto reasonS = senderClient.Factory().PasswordPrompt(__func__);
+
+    cookie = ot::identifier::Generic::Random()->str();
+    command.set_cookie(cookie);
+    command.set_type(proto::RPCCOMMAND_ADDCLIENTSESSION);
+    command.set_session(-1);
+    response = ot.RPC(command);
+
+    ASSERT_TRUE(proto::Validate(response, VERBOSE));
+    ASSERT_EQ(1, response.status_size());
+    ASSERT_EQ(proto::RPCRESPONSE_SUCCESS, response.status(0).code());
+
+    auto& receiverClient =
+        ot.Client(static_cast<int>(get_index(response.session())));
+    auto reasonR = receiverClient.Factory().PasswordPrompt(__func__);
+
+    auto client_a_server_contract =
+        senderClient.Wallet().Server(intro_server_contract->PublicContract());
+    senderClient.OTX().SetIntroductionServer(client_a_server_contract);
+
+    auto client_b_server_contract =
+        receiverClient.Wallet().Server(intro_server_contract->PublicContract());
+    receiverClient.OTX().SetIntroductionServer(client_b_server_contract);
+
+    auto started = notification_socket_->get().Start(
+        ot.ZMQ().BuildEndpoint("rpc/push", -1, 1));
+
+    ASSERT_TRUE(started);
+
+    sender_nym_id_ = senderClient.Wallet().Nym(reasonS, TEST_NYM_4)->ID();
+
+    receiver_nym_id_ = receiverClient.Wallet().Nym(reasonR, TEST_NYM_5)->ID();
+
+    auto unit_definition = senderClient.Wallet().UnitDefinition(
+        sender_nym_id_->str(),
+        "gdollar",
+        "GoogleTestDollar",
+        "G",
+        "Google Test Dollars",
+        "GTD",
+        2,
+        "gcent",
+        ot::UnitType::Usd,
+        reasonS);
+    unit_definition_id_ =
+        ot::identifier::UnitDefinition::Factory(unit_definition->ID()->str());
+    intro_server_ = intro_server.Instance();
+    server_ = server.Instance();
+    sender_session_ = senderClient.Instance();
+    receiver_session_ = receiverClient.Instance();
+}
+
+}  // namespace ottest
---
+++ tests/ottest/fixtures/rpc/RpcAsync.hpp
@@ -0,0 +1,95 @@
+// Copyright (c) 2010-2023 The Open-Transactions developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <APIArgument.pb.h>
+#include <AcceptPendingPayment.pb.h>
+#include <AccountEvent.pb.h>
+#include <PaymentWorkflowEnums.pb.h>
+#include <RPCCommand.pb.h>
+#include <RPCEnums.pb.h>
+#include <RPCPush.pb.h>
+#include <RPCResponse.pb.h>
+#include <RPCStatus.pb.h>
+#include <RPCTask.pb.h>
+#include <SendPayment.pb.h>
+#include <TaskComplete.pb.h>
+#include <gtest/gtest.h>
+#include <opentxs/opentxs.hpp>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#include <ctime>
+#include <functional>
+#include <future>
+#include <iosfwd>
+#include <memory>
+#include <utility>
+
+#include "internal/serialization/protobuf/Check.hpp"
+#include "internal/serialization/protobuf/verify/RPCPush.hpp"
+#include "internal/serialization/protobuf/verify/RPCResponse.hpp"
+#include "ottest/Basic.hpp"
+#include "ottest/fixtures/common/Base.hpp"
+
+#define COMMAND_VERSION 3
+#define RESPONSE_VERSION 3
+#define ACCOUNTEVENT_VERSION 2
+#define APIARG_VERSION 1
+#define TEST_NYM_4 "testNym4"
+#define TEST_NYM_5 "testNym5"
+#define TEST_NYM_6 "testNym6"
+#define ISSUER_ACCOUNT_LABEL "issuer account"
+#define USER_ACCOUNT_LABEL "user account"
+
+namespace ottest
+{
+namespace ot = opentxs;
+
+class RpcAsync : virtual public Base
+{
+public:
+    using PushChecker = std::function<bool(const ot::proto::RPCPush&)>;
+
+    RpcAsync();
+
+protected:
+    static int sender_session_;
+    static int receiver_session_;
+    static ot::identifier::Generic destination_account_id_;
+    static int intro_server_;
+    static std::unique_ptr<OTZMQListenCallback> notification_callback_;
+    static std::unique_ptr<OTZMQSubscribeSocket> notification_socket_;
+    static ot::identifier::Nym receiver_nym_id_;
+    static ot::identifier::Nym sender_nym_id_;
+    static int server_;
+    static ot::identifier::UnitDefinition unit_definition_id_;
+    static ot::identifier::Generic workflow_id_;
+    static ot::identifier::Notary intro_server_id_;
+    static ot::identifier::Notary server_id_;
+
+    static bool check_push_results(const ot::UnallocatedVector<bool>& results);
+    static void cleanup();
+    static std::size_t get_index(const std::int32_t instance);
+    static const api::Session& get_session(const std::int32_t instance);
+    static void process_notification(
+        const ot::network::zeromq::Message&& incoming);
+    static bool default_push_callback(const ot::proto::RPCPush& push);
+    static void setup();
+
+    proto::RPCCommand init(proto::RPCCommandType commandtype);
+    std::future<ot::UnallocatedVector<bool>> set_push_checker(
+        PushChecker func,
+        std::size_t count = 1);
+
+private:
+    static PushChecker push_checker_;
+    static std::promise<ot::UnallocatedVector<bool>> push_received_;
+    static ot::UnallocatedVector<bool> push_results_;
+    static std::size_t push_results_count_;
+};
+
+}  // namespace ottest
