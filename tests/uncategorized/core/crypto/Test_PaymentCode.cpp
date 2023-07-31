d5e02bd16a348b1224dc9a0a5f4331e222dbd2fa
FellowTraveler <F3llowTraveler@gmail.com>
2023-07-30 19:02:23 -0700

    PaymentCode test migration

--- tests/uncategorized/core/crypto/Test_PaymentCode.cpp
+++ tests/uncategorized/core/crypto/Test_PaymentCode.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010-2022 The Open-Transactions developers
+// Copyright (c) 2010-2023 The Open-Transactions developers
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -10,7 +10,7 @@
 #include <string_view>
 #include <utility>
 
-#include "ottest/env/OTTestEnvironment.hpp"
+#include "ottest/fixtures/core/PaymentCode.hpp"
 
 namespace ot = opentxs;
 
@@ -18,117 +18,9 @@ namespace ottest
 {
 using namespace std::literals;
 
-class Test_PaymentCode : public ::testing::Test
-{
-public:
-    static const bool have_hd_;
-
-    const ot::api::session::Client& api_;
-    ot::PasswordPrompt reason_;
-    ot::UnallocatedCString seed_;
-    ot::crypto::SeedID fingerprint_;
-    ot::UnallocatedCString nym_id_0_, paycode_0_, nym_id_1_, paycode_1_,
-        nym_id_2_, paycode_2_, nym_id_3_, paycode_3_;
-    ot::NymData nym_data_0_, nym_data_1_, nym_data_2_, nym_data_3_;
-    ot::UnitType currency_ = ot::UnitType::Bch;
-    ot::UnitType currency_2_ = ot::UnitType::Btc;
-
-    /* Is evaluated every test, therefore indexes are fixed to 0,1,2,3 */
-    Test_PaymentCode()
-        : api_(OTTestEnvironment::GetOT().StartClientSession(0))
-        , reason_(api_.Factory().PasswordPrompt(__func__))
-        , seed_(
-              "trim thunder unveil reduce crop cradle zone inquiry anchor skate property fringe obey butter text tank drama palm guilt pudding laundry stay axis prosper"sv)
-        , fingerprint_(api_.Crypto().Seed().ImportSeed(
-              api_.Factory().SecretFromText(seed_),
-              api_.Factory().SecretFromText(""sv),
-              opentxs::crypto::SeedStyle::BIP39,
-              opentxs::crypto::Language::en,
-              api_.Factory().PasswordPrompt("Importing a BIP-39 seed")))
-        , nym_id_0_(api_.Wallet()
-                        .Nym(
-                            {api_.Factory(), fingerprint_, 0, 1},
-                            reason_,
-                            "PaycodeNym")
-                        ->ID()
-                        .asBase58(api_.Crypto()))
-        , paycode_0_(
-              "PM8TJhB2CxWDqR8c5y4kWoJwSGRNYaVATdJM85kqfn2dZ9TdSihbFJraQzjYUMYxbsrnMfjPK6oZFAPQ1tWqzwTfKbtunvLFCzDJFVXVGbUAKxhsz7P5"sv)
-        , nym_id_1_(api_.Wallet()
-                        .Nym(
-                            {api_.Factory(), fingerprint_, 1, 1},
-                            reason_,
-                            "PaycodeNym_1")
-                        ->ID()
-                        .asBase58(api_.Crypto()))
-        , paycode_1_(
-              "PM8TJWedQTvxaoJpt9Wh25HR54oj5vmor6arAByFk4UTgUh1Tna2srsZLUo2xS3ViBot1ftf4p8ZUN8khB2zvViHXZkrwkfjcePSeEgsYapESKywge9F"sv)
-        , nym_id_2_(api_.Wallet()
-                        .Nym(
-                            {api_.Factory(), fingerprint_, 2, 1},
-                            reason_,
-                            "PaycodeNym_2")
-                        ->ID()
-                        .asBase58(api_.Crypto()))
-        , paycode_2_(
-              "PM8TJQmrQ4tSY6Gad59UpzqR8MRMesSYMKXvpMuzdDHByfRXVgvVdiqD5NmjoEH9V6ZrofFVViBwSg9dvVcP8R2CU1pXejhVQQj3XsWk8sLhAsspqk8F"sv)
-        , nym_id_3_(api_.Wallet()
-                        .Nym(
-                            {api_.Factory(), fingerprint_, 3, 1},
-                            reason_,
-                            "PaycodeNym_3")
-                        ->ID()
-                        .asBase58(api_.Crypto()))
-        , paycode_3_(
-              "PM8TJbNzqDcdqCcpkMLLa9H83CjoWdHMTQ4Lk11qSpThkyrmDFA4AeGd2kFeLK2sT6UVXy2jwWABsfLd7JmcS4hMAy9zUdWRFRhmu33RiRJCS6qRmGew"sv)
-        , nym_data_0_(api_.Wallet().mutable_Nym(
-              api_.Factory().NymIDFromBase58(nym_id_0_),
-              reason_))
-        , nym_data_1_(api_.Wallet().mutable_Nym(
-              api_.Factory().NymIDFromBase58(nym_id_1_),
-              reason_))
-        , nym_data_2_(api_.Wallet().mutable_Nym(
-              api_.Factory().NymIDFromBase58(nym_id_2_),
-              reason_))
-        , nym_data_3_(api_.Wallet().mutable_Nym(
-              api_.Factory().NymIDFromBase58(nym_id_3_),
-              reason_))
-    {
-        nym_data_0_.AddPaymentCode(paycode_0_, currency_, true, true, reason_);
-
-        nym_data_1_.AddPaymentCode(paycode_0_, currency_, true, true, reason_);
-        nym_data_1_.AddPaymentCode(
-            paycode_1_, currency_, true, false, reason_);  // reset nymdata_1 to
-                                                           // paymentcode_1
-
-        nym_data_2_.AddPaymentCode(
-            paycode_2_, currency_2_, false, true, reason_);  // nymdata_2 resets
-                                                             // paycode_2 to
-                                                             // primary
-        nym_data_2_.AddPaymentCode(
-            paycode_0_, currency_2_, false, true, reason_);  // fail to reset
-                                                             // nymdata_2 with
-                                                             // primary = false
-
-        nym_data_3_.AddPaymentCode(
-            paycode_3_, currency_2_, false, false, reason_);  // nymdata_3
-                                                              // resets
-                                                              // paycode_3 to
-                                                              // primary
-        nym_data_3_.AddPaymentCode(
-            paycode_0_, currency_2_, false, false, reason_);  // fail to be
-                                                              // primary
-    }
-};
-
-const bool Test_PaymentCode::have_hd_{
-    ot::api::crypto::HaveHDKeys() &&
-    ot::api::crypto::HaveSupport(ot::crypto::asymmetric::Algorithm::Secp256k1)
-
-};
 /* Test: Gets the last paymentcode to be set as primary
  */
-TEST_F(Test_PaymentCode, primary_paycodes)
+TEST_F(PaymentCode, primary_paycodes)
 {
     EXPECT_STREQ(
         paycode_0_.c_str(),
@@ -166,7 +58,7 @@ TEST_F(Test_PaymentCode, primary_paycodes)
 
 /* Test: by setting primary = true it resets best payment code
  */
-TEST_F(Test_PaymentCode, test_new_primary)
+TEST_F(PaymentCode, test_new_primary)
 {
     EXPECT_STREQ(
         paycode_0_.c_str(), nym_data_0_.PaymentCode(currency_).c_str());
@@ -184,7 +76,7 @@ TEST_F(Test_PaymentCode, test_new_primary)
 
 /* Test: by setting primary = false it should not override a previous primary
  */
-TEST_F(Test_PaymentCode, test_secondary_doesnt_replace)
+TEST_F(PaymentCode, test_secondary_doesnt_replace)
 {
     EXPECT_STREQ(
         paycode_0_.c_str(), nym_data_0_.PaymentCode(currency_).c_str());
@@ -200,7 +92,7 @@ TEST_F(Test_PaymentCode, test_secondary_doesnt_replace)
 
 /* Test: Valid paycodes
  */
-TEST_F(Test_PaymentCode, valid_paycodes)
+TEST_F(PaymentCode, valid_paycodes)
 {
     ASSERT_TRUE(api_.Factory().PaymentCodeFromBase58(paycode_0_).Valid());
     ASSERT_TRUE(api_.Factory().PaymentCodeFromBase58(paycode_1_).Valid());
@@ -210,7 +102,7 @@ TEST_F(Test_PaymentCode, valid_paycodes)
 
 /* Test: Invalid paycodes should not be saved
  */
-TEST_F(Test_PaymentCode, empty_paycode)
+TEST_F(PaymentCode, empty_paycode)
 {
     EXPECT_STREQ(
         paycode_0_.c_str(), nym_data_0_.PaymentCode(currency_).c_str());
@@ -237,7 +129,7 @@ TEST_F(Test_PaymentCode, empty_paycode)
 
 /* Test: Base58 encoding
  */
-TEST_F(Test_PaymentCode, asBase58)
+TEST_F(PaymentCode, asBase58)
 {
     auto pcode = api_.Factory().PaymentCodeFromBase58(paycode_0_);
     EXPECT_STREQ(paycode_0_.c_str(), pcode.asBase58().c_str());
@@ -245,7 +137,7 @@ TEST_F(Test_PaymentCode, asBase58)
 
 /* Test: Factory methods create the same paycode
  */
-TEST_F(Test_PaymentCode, factory)
+TEST_F(PaymentCode, factory)
 {
     // Factory 0: PaymentCode&
     auto factory_0 = api_.Factory().PaymentCodeFromBase58(paycode_0_);
@@ -297,7 +189,7 @@ TEST_F(Test_PaymentCode, factory)
 
 /* Test: factory method with nym has private key
  */
-TEST_F(Test_PaymentCode, factory_seed_nym)
+TEST_F(PaymentCode, factory_seed_nym)
 {
     if (have_hd_) {
         auto seed = api_.Crypto().Seed().DefaultSeed().first;
@@ -322,7 +214,7 @@ TEST_F(Test_PaymentCode, factory_seed_nym)
     }
 }
 
-TEST_F(Test_PaymentCode, nymid)
+TEST_F(PaymentCode, nymid)
 {
     EXPECT_EQ(
         api_.Factory().PaymentCodeFromBase58(paycode_0_).ID(),
