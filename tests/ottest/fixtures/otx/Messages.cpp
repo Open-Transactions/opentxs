39256d141a671cdae51a293f745a7e619ff4a603
FellowTraveler <F3llowTraveler@gmail.com>
2023-07-31 00:25:48 -0700

    otx/Messages migration

---
+++ tests/ottest/fixtures/otx/Messages.cpp
@@ -0,0 +1,74 @@
+// Copyright (c) 2010-2023 The Open-Transactions developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ottest/fixtures/otx/Messages.hpp"  // IWYU pragma: associated
+
+#include <opentxs/opentxs.hpp>
+#include <memory>
+#include <string_view>
+
+#include "internal/api/session/Wallet.hpp"
+#include "internal/core/contract/ServerContract.hpp"
+#include "internal/util/LogMacros.hpp"
+#include "ottest/env/OTTestEnvironment.hpp"
+
+namespace ottest
+{
+namespace ot = opentxs;
+using namespace std::literals;
+
+bool init_{false};
+
+const ot::crypto::SeedID Messages::SeedA_{};
+const ot::UnallocatedCString Messages::Alice_{""};
+const ot::identifier::Nym Messages::alice_nym_id_{};
+
+Messages::Messages()
+    : client_(dynamic_cast<const ot::api::session::Client&>(
+          OTTestEnvironment::GetOT().StartClientSession(0)))
+    , server_(dynamic_cast<const ot::api::session::Notary&>(
+          OTTestEnvironment::GetOT().StartNotarySession(0)))
+    , reason_c_(client_.Factory().PasswordPrompt(__func__))
+    , reason_s_(server_.Factory().PasswordPrompt(__func__))
+    , server_id_(server_.ID())
+    , server_contract_(server_.Wallet().Internal().Server(server_id_))
+{
+    if (false == init_) { init(); }
+}
+
+void Messages::import_server_contract(
+    const ot::contract::Server& contract,
+    const ot::api::session::Client& client)
+{
+    auto bytes = ot::Space{};
+    server_contract_->Serialize(ot::writer(bytes), true);
+    auto clientVersion = client.Wallet().Internal().Server(ot::reader(bytes));
+    client.OTX().SetIntroductionServer(clientVersion);
+}
+
+void Messages::init()
+{
+    const_cast<ot::crypto::SeedID&>(SeedA_) = client_.Crypto().Seed().ImportSeed(
+        client_.Factory().SecretFromText(
+            "spike nominee miss inquiry fee nothing belt list other daughter leave valley twelve gossip paper"sv),
+        client_.Factory().SecretFromText(""sv),
+        opentxs::crypto::SeedStyle::BIP39,
+        opentxs::crypto::Language::en,
+        client_.Factory().PasswordPrompt("Importing a BIP-39 seed"));
+    const_cast<ot::identifier::Nym&>(alice_nym_id_) =
+        client_.Wallet()
+            .Nym({client_.Factory(), SeedA_, 0}, reason_c_, "Alice")
+            ->ID();
+    const_cast<ot::UnallocatedCString&>(Alice_) =
+        alice_nym_id_.asBase58(client_.Crypto());
+
+    OT_ASSERT(false == server_id_.empty());
+
+    import_server_contract(server_contract_, client_);
+
+    init_ = true;
+}
+
+}  // namespace ottest
--- tests/uncategorized/otx/Test_Messages.cpp
+++ tests/uncategorized/otx/Test_Messages.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010-2022 The Open-Transactions developers
+// Copyright (c) 2010-2023 The Open-Transactions developers
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -6,88 +6,15 @@
 #include <gtest/gtest.h>
 #include <opentxs/opentxs.hpp>
 #include <memory>
-#include <string_view>
 
-#include "internal/api/session/Wallet.hpp"
-#include "internal/core/contract/ServerContract.hpp"
-#include "internal/util/LogMacros.hpp"
-#include "ottest/env/OTTestEnvironment.hpp"
+#include "ottest/fixtures/otx/Messages.hpp"
 
 namespace ot = opentxs;
 
 namespace ottest
 {
-using namespace std::literals;
 
-bool init_{false};
-
-class Test_Messages : public ::testing::Test
-{
-public:
-    static const ot::crypto::SeedID SeedA_;
-    static const ot::UnallocatedCString Alice_;
-    static const ot::identifier::Nym alice_nym_id_;
-
-    const ot::api::session::Client& client_;
-    const ot::api::session::Notary& server_;
-    ot::PasswordPrompt reason_c_;
-    ot::PasswordPrompt reason_s_;
-    const ot::identifier::Notary& server_id_;
-    const ot::OTServerContract server_contract_;
-
-    Test_Messages()
-        : client_(dynamic_cast<const ot::api::session::Client&>(
-              OTTestEnvironment::GetOT().StartClientSession(0)))
-        , server_(dynamic_cast<const ot::api::session::Notary&>(
-              OTTestEnvironment::GetOT().StartNotarySession(0)))
-        , reason_c_(client_.Factory().PasswordPrompt(__func__))
-        , reason_s_(server_.Factory().PasswordPrompt(__func__))
-        , server_id_(server_.ID())
-        , server_contract_(server_.Wallet().Internal().Server(server_id_))
-    {
-        if (false == init_) { init(); }
-    }
-
-    void import_server_contract(
-        const ot::contract::Server& contract,
-        const ot::api::session::Client& client)
-    {
-        auto bytes = ot::Space{};
-        server_contract_->Serialize(ot::writer(bytes), true);
-        auto clientVersion =
-            client.Wallet().Internal().Server(ot::reader(bytes));
-        client.OTX().SetIntroductionServer(clientVersion);
-    }
-
-    void init()
-    {
-        const_cast<ot::crypto::SeedID&>(SeedA_) = client_.Crypto().Seed().ImportSeed(
-            client_.Factory().SecretFromText(
-                "spike nominee miss inquiry fee nothing belt list other daughter leave valley twelve gossip paper"sv),
-            client_.Factory().SecretFromText(""sv),
-            opentxs::crypto::SeedStyle::BIP39,
-            opentxs::crypto::Language::en,
-            client_.Factory().PasswordPrompt("Importing a BIP-39 seed"));
-        const_cast<ot::identifier::Nym&>(alice_nym_id_) =
-            client_.Wallet()
-                .Nym({client_.Factory(), SeedA_, 0}, reason_c_, "Alice")
-                ->ID();
-        const_cast<ot::UnallocatedCString&>(Alice_) =
-            alice_nym_id_.asBase58(client_.Crypto());
-
-        OT_ASSERT(false == server_id_.empty());
-
-        import_server_contract(server_contract_, client_);
-
-        init_ = true;
-    }
-};
-
-const ot::crypto::SeedID Test_Messages::SeedA_{};
-const ot::UnallocatedCString Test_Messages::Alice_{""};
-const ot::identifier::Nym Test_Messages::alice_nym_id_{};
-
-TEST_F(Test_Messages, activateRequest)
+TEST_F(Messages, activateRequest)
 {
     const ot::otx::ServerRequestType type{ot::otx::ServerRequestType::Activate};
     auto requestID = ot::identifier::Generic{};
@@ -133,7 +60,7 @@ TEST_F(Test_Messages, activateRequest)
     EXPECT_TRUE(serverCopy.Validate());
 }
 
-TEST_F(Test_Messages, pushReply)
+TEST_F(Messages, pushReply)
 {
     const ot::UnallocatedCString payload{"TEST PAYLOAD"};
     const ot::otx::ServerReplyType type{ot::otx::ServerReplyType::Push};
