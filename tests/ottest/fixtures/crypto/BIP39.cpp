7ac6c94e0c303da436f95edd542e5378dea0d64e
FellowTraveler <F3llowTraveler@gmail.com>
2023-09-06 04:38:28 -0700

    crypto/BIP39 test migration

---
+++ tests/ottest/fixtures/crypto/BIP39.cpp
@@ -0,0 +1,126 @@
+// Copyright (c) 2010-2023 The Open-Transactions developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ottest/fixtures/crypto/BIP39.hpp"  // IWYU pragma: associated
+
+#include <gtest/gtest.h>
+#include <cctype>
+#include <cstddef>
+#include <functional>
+#include <string_view>
+
+#include "internal/util/P0330.hpp"
+#include "opentxs/opentxs.hpp"
+#include "ottest/env/OTTestEnvironment.hpp"
+
+namespace ottest
+{
+namespace ot = opentxs;
+
+ot::UnallocatedSet<ot::crypto::SeedID> BIP39::generated_seeds_{};
+
+auto BIP39::expected_seed_languages() noexcept
+    -> const ot::UnallocatedMap<ot::crypto::SeedStyle, Languages>&
+{
+    using Language = ot::crypto::Language;
+    static const auto data =
+        ot::UnallocatedMap<ot::crypto::SeedStyle, Languages>{
+            {ot::crypto::SeedStyle::BIP39,
+             {
+                 {Language::en, "English"},
+             }},
+            {ot::crypto::SeedStyle::PKT,
+             {
+                 {Language::en, "English"},
+             }},
+        };
+
+    return data;
+}
+auto BIP39::expected_seed_strength() noexcept
+    -> const ot::UnallocatedMap<ot::crypto::SeedStyle, Strengths>&
+{
+    using Style = ot::crypto::SeedStyle;
+    using Strength = ot::crypto::SeedStrength;
+    static const auto data =
+        ot::UnallocatedMap<ot::crypto::SeedStyle, Strengths>{
+            {Style::BIP39,
+             {
+                 {Strength::Twelve, "12 words"},
+                 {Strength::Fifteen, "15 words"},
+                 {Strength::Eighteen, "18 words"},
+                 {Strength::TwentyOne, "21 words"},
+                 {Strength::TwentyFour, "24 words"},
+             }},
+            {Style::PKT,
+             {
+                 {Strength::Fifteen, "15 words"},
+             }},
+        };
+
+    return data;
+}
+auto BIP39::expected_seed_types() noexcept -> const Types&
+{
+    static const auto data = Types{
+        {ot::crypto::SeedStyle::BIP39, "BIP-39"},
+        {ot::crypto::SeedStyle::PKT, "Legacy pktwallet"},
+    };
+
+    return data;
+}
+
+auto BIP39::word_count(const ot::UnallocatedCString& in) noexcept -> std::size_t
+{
+    if (0 == in.size()) { return 0; }
+
+    auto word = false;
+    using namespace opentxs::literals;
+    using namespace std::literals;
+    auto count = 0_uz;
+
+    for (const auto c : in) {
+        if (std::isspace(c)) {
+            if (word) {
+                word = false;
+            } else {
+                continue;
+            }
+        } else {
+            if (word) {
+                continue;
+            } else {
+                word = true;
+                ++count;
+            }
+        }
+    }
+
+    return count;
+}
+
+auto BIP39::generate_words(const ot::crypto::SeedStrength count) const
+    -> std::size_t
+{
+    const auto fingerprint =
+        api_.Crypto().Seed().NewSeed(type_, lang_, count, reason_);
+
+    EXPECT_EQ(generated_seeds_.count(fingerprint), 0);
+
+    if (0 < generated_seeds_.count(fingerprint)) { return 0; }
+
+    generated_seeds_.insert(fingerprint);
+
+    const auto words = api_.Crypto().Seed().Words(fingerprint, reason_);
+
+    return word_count(words);
+}
+
+BIP39::BIP39()
+    : api_(OTTestEnvironment::GetOT().StartClientSession(0))
+    , reason_(api_.Factory().PasswordPrompt(__func__))
+{
+}
+}  // namespace ottest
--- tests/uncategorized/crypto/Test_BIP39.cpp
+++ tests/uncategorized/crypto/Test_BIP39.cpp
@@ -1,149 +1,23 @@
-// Copyright (c) 2010-2022 The Open-Transactions developers
+// Copyright (c) 2010-2023 The Open-Transactions developers
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include <gtest/gtest.h>
 #include <opentxs/opentxs.hpp>
-#include <cctype>
 #include <cstddef>
 #include <functional>
 #include <memory>
 #include <optional>
-#include <string_view>
 
 #include "crypto/Bip39.hpp"
-#include "internal/util/P0330.hpp"
-#include "ottest/env/OTTestEnvironment.hpp"
+#include "ottest/fixtures/crypto/BIP39.hpp"
 
 namespace ot = opentxs;
 
 namespace ottest
 {
-class Test_BIP39 : public ::testing::Test
-{
-public:
-    static constexpr auto type_{ot::crypto::SeedStyle::BIP39};
-    static constexpr auto lang_{ot::crypto::Language::en};
-    static ot::UnallocatedSet<ot::crypto::SeedID> generated_seeds_;
-
-    const ot::api::session::Client& api_;
-    const ot::PasswordPrompt reason_;
-    using Languages =
-        ot::UnallocatedMap<ot::crypto::Language, ot::UnallocatedCString>;
-    using Strengths =
-        ot::UnallocatedMap<ot::crypto::SeedStrength, ot::UnallocatedCString>;
-    using Types =
-        ot::UnallocatedMap<ot::crypto::SeedStyle, ot::UnallocatedCString>;
-
-    static auto expected_seed_languages() noexcept
-        -> const ot::UnallocatedMap<ot::crypto::SeedStyle, Languages>&
-    {
-        using Language = ot::crypto::Language;
-        static const auto data =
-            ot::UnallocatedMap<ot::crypto::SeedStyle, Languages>{
-                {ot::crypto::SeedStyle::BIP39,
-                 {
-                     {Language::en, "English"},
-                 }},
-                {ot::crypto::SeedStyle::PKT,
-                 {
-                     {Language::en, "English"},
-                 }},
-            };
-
-        return data;
-    }
-    static auto expected_seed_strength() noexcept
-        -> const ot::UnallocatedMap<ot::crypto::SeedStyle, Strengths>&
-    {
-        using Style = ot::crypto::SeedStyle;
-        using Strength = ot::crypto::SeedStrength;
-        static const auto data =
-            ot::UnallocatedMap<ot::crypto::SeedStyle, Strengths>{
-                {Style::BIP39,
-                 {
-                     {Strength::Twelve, "12 words"},
-                     {Strength::Fifteen, "15 words"},
-                     {Strength::Eighteen, "18 words"},
-                     {Strength::TwentyOne, "21 words"},
-                     {Strength::TwentyFour, "24 words"},
-                 }},
-                {Style::PKT,
-                 {
-                     {Strength::Fifteen, "15 words"},
-                 }},
-            };
-
-        return data;
-    }
-    static auto expected_seed_types() noexcept -> const Types&
-    {
-        static const auto data = Types{
-            {ot::crypto::SeedStyle::BIP39, "BIP-39"},
-            {ot::crypto::SeedStyle::PKT, "Legacy pktwallet"},
-        };
-
-        return data;
-    }
-
-    static auto word_count(const ot::UnallocatedCString& in) noexcept
-        -> std::size_t
-    {
-        if (0 == in.size()) { return 0; }
-
-        auto word = false;
-        using namespace opentxs::literals;
-        using namespace std::literals;
-        auto count = 0_uz;
-
-        for (const auto c : in) {
-            if (std::isspace(c)) {
-                if (word) {
-                    word = false;
-                } else {
-                    continue;
-                }
-            } else {
-                if (word) {
-                    continue;
-                } else {
-                    word = true;
-                    ++count;
-                }
-            }
-        }
-
-        return count;
-    }
-
-    auto generate_words(const ot::crypto::SeedStrength count) const
-        -> std::size_t
-    {
-        const auto fingerprint =
-            api_.Crypto().Seed().NewSeed(type_, lang_, count, reason_);
-
-        EXPECT_EQ(generated_seeds_.count(fingerprint), 0);
-
-        if (0 < generated_seeds_.count(fingerprint)) { return 0; }
-
-        generated_seeds_.insert(fingerprint);
-
-        const auto words = api_.Crypto().Seed().Words(fingerprint, reason_);
-
-        return word_count(words);
-    }
-
-    Test_BIP39()
-        : api_(OTTestEnvironment::GetOT().StartClientSession(0))
-        , reason_(api_.Factory().PasswordPrompt(__func__))
-    {
-    }
-};
-
-ot::UnallocatedSet<ot::crypto::SeedID> Test_BIP39::generated_seeds_{};
-
-TEST_F(Test_BIP39, seed_types)
+TEST_F(BIP39, seed_types)
 {
     const auto& expected = expected_seed_types();
     const auto test = api_.Crypto().Seed().AllowedSeedTypes();
@@ -159,7 +33,7 @@ TEST_F(Test_BIP39, seed_types)
     }
 }
 
-TEST_F(Test_BIP39, seed_languages)
+TEST_F(BIP39, seed_languages)
 {
     for (const auto& [type, expected] : expected_seed_languages()) {
         const auto test = api_.Crypto().Seed().AllowedLanguages(type);
@@ -176,7 +50,7 @@ TEST_F(Test_BIP39, seed_languages)
     }
 }
 
-TEST_F(Test_BIP39, seed_strength)
+TEST_F(BIP39, seed_strength)
 {
     for (const auto& [type, expected] : expected_seed_strength()) {
         const auto test = api_.Crypto().Seed().AllowedSeedStrength(type);
@@ -193,7 +67,7 @@ TEST_F(Test_BIP39, seed_strength)
     }
 }
 
-TEST_F(Test_BIP39, word_count)
+TEST_F(BIP39, word_count)
 {
     static const auto vector =
         ot::UnallocatedMap<ot::UnallocatedCString, std::size_t>{
@@ -217,12 +91,12 @@ TEST_F(Test_BIP39, word_count)
     }
 }
 
-TEST_F(Test_BIP39, longest_en)
+TEST_F(BIP39, longest_en)
 {
     EXPECT_EQ(api_.Crypto().Seed().LongestWord(type_, lang_), 8);
 }
 
-TEST_F(Test_BIP39, twelve_words)
+TEST_F(BIP39, twelve_words)
 {
     const auto& api = api_.Crypto().Seed();
     constexpr auto strength{ot::crypto::SeedStrength::Twelve};
@@ -232,7 +106,7 @@ TEST_F(Test_BIP39, twelve_words)
     EXPECT_EQ(api.WordCount(type_, strength), words);
 }
 
-TEST_F(Test_BIP39, fifteen_words)
+TEST_F(BIP39, fifteen_words)
 {
     const auto& api = api_.Crypto().Seed();
     constexpr auto strength{ot::crypto::SeedStrength::Fifteen};
@@ -242,7 +116,7 @@ TEST_F(Test_BIP39, fifteen_words)
     EXPECT_EQ(api.WordCount(type_, strength), words);
 }
 
-TEST_F(Test_BIP39, eighteen_words)
+TEST_F(BIP39, eighteen_words)
 {
     const auto& api = api_.Crypto().Seed();
     constexpr auto strength{ot::crypto::SeedStrength::Eighteen};
@@ -252,7 +126,7 @@ TEST_F(Test_BIP39, eighteen_words)
     EXPECT_EQ(api.WordCount(type_, strength), words);
 }
 
-TEST_F(Test_BIP39, twentyone_words)
+TEST_F(BIP39, twentyone_words)
 {
     const auto& api = api_.Crypto().Seed();
     constexpr auto strength{ot::crypto::SeedStrength::TwentyOne};
@@ -262,7 +136,7 @@ TEST_F(Test_BIP39, twentyone_words)
     EXPECT_EQ(api.WordCount(type_, strength), words);
 }
 
-TEST_F(Test_BIP39, twentyfour_words)
+TEST_F(BIP39, twentyfour_words)
 {
     const auto& api = api_.Crypto().Seed();
     constexpr auto strength{ot::crypto::SeedStrength::TwentyFour};
@@ -272,7 +146,7 @@ TEST_F(Test_BIP39, twentyfour_words)
     EXPECT_EQ(api.WordCount(type_, strength), words);
 }
 
-TEST_F(Test_BIP39, match_a)
+TEST_F(BIP39, match_a)
 {
     const auto test = ot::UnallocatedCString{"a"};
     const auto expected = ot::UnallocatedVector<ot::UnallocatedCString>{
@@ -306,7 +180,7 @@ TEST_F(Test_BIP39, match_a)
     EXPECT_EQ(suggestions, expected);
 }
 
-TEST_F(Test_BIP39, match_ar)
+TEST_F(BIP39, match_ar)
 {
     const auto test = ot::UnallocatedCString{"ar"};
     const auto expected = ot::UnallocatedVector<ot::UnallocatedCString>{
@@ -334,7 +208,7 @@ TEST_F(Test_BIP39, match_ar)
     EXPECT_EQ(suggestions, expected);
 }
 
-TEST_F(Test_BIP39, match_arr)
+TEST_F(BIP39, match_arr)
 {
     const auto test = ot::UnallocatedCString{"arr"};
     const auto expected = ot::UnallocatedVector<ot::UnallocatedCString>{
@@ -345,7 +219,7 @@ TEST_F(Test_BIP39, match_arr)
     EXPECT_EQ(suggestions, expected);
 }
 
-TEST_F(Test_BIP39, match_arri)
+TEST_F(BIP39, match_arri)
 {
     const auto test = ot::UnallocatedCString{"arri"};
     const auto expected =
@@ -356,7 +230,7 @@ TEST_F(Test_BIP39, match_arri)
     EXPECT_EQ(suggestions, expected);
 }
 
-TEST_F(Test_BIP39, match_arrive)
+TEST_F(BIP39, match_arrive)
 {
     const auto test = ot::UnallocatedCString{"arrive"};
     const auto expected =
@@ -367,7 +241,7 @@ TEST_F(Test_BIP39, match_arrive)
     EXPECT_EQ(suggestions, expected);
 }
 
-TEST_F(Test_BIP39, match_arrived)
+TEST_F(BIP39, match_arrived)
 {
     const auto test = ot::UnallocatedCString{"arrived"};
     const auto expected = ot::UnallocatedVector<ot::UnallocatedCString>{};
@@ -377,7 +251,7 @@ TEST_F(Test_BIP39, match_arrived)
     EXPECT_EQ(suggestions, expected);
 }
 
-TEST_F(Test_BIP39, match_axe)
+TEST_F(BIP39, match_axe)
 {
     const auto test = ot::UnallocatedCString{"axe"};
     const auto expected = ot::UnallocatedVector<ot::UnallocatedCString>{};
@@ -387,7 +261,7 @@ TEST_F(Test_BIP39, match_axe)
     EXPECT_EQ(suggestions, expected);
 }
 
-TEST_F(Test_BIP39, match_empty_string)
+TEST_F(BIP39, match_empty_string)
 {
     const auto test = ot::UnallocatedCString{""};
     const auto expected = ot::UnallocatedVector<ot::UnallocatedCString>{};
@@ -397,7 +271,7 @@ TEST_F(Test_BIP39, match_empty_string)
     EXPECT_EQ(suggestions, expected);
 }
 
-TEST_F(Test_BIP39, validate_en_list)
+TEST_F(BIP39, validate_en_list)
 {
     using Bip39 = opentxs::crypto::implementation::Bip39;
 
@@ -409,7 +283,7 @@ TEST_F(Test_BIP39, validate_en_list)
     }
 }
 
-TEST_F(Test_BIP39, pkt_seed_import)
+TEST_F(BIP39, pkt_seed_import)
 {
     static constexpr auto phrase{
         "forum school old approve bubble warfare robust figure pact glance "
